87a6dacf8a953ad1cf50a21977aaf6fd8b64f277
Sven Duzont <sven.duzont@gmail.com>
2008-11-03 21:23:27 +0000

    Module split first step

---
+++ yass-webapp/src/main/java/org/yass/struts/library/GetTree.java
@@ -0,0 +1,108 @@
+/*
+ Copyright (c) 2008 Sven Duzont sven.duzont@gmail.com> All rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is furnished
+ to do so, subject to the following conditions: The above copyright notice
+ and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS",
+ WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.yass.struts.library;
+
+import java.util.Collection;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.yass.YassConstants;
+import org.yass.domain.Track;
+import org.yass.domain.TrackInfo;
+import org.yass.struts.YassAction;
+
+public class GetTree extends YassAction implements YassConstants {
+
+	private static final long serialVersionUID = 3411435373847531163L;
+
+	private final static Element makeNodeFromProp(final Document doc, final TrackInfo trackInfo) {
+		final Element node = doc.createElement(trackInfo.getType());
+		node.setAttribute("id", "" + trackInfo.getId());
+		node.setAttribute("value", trackInfo.getValue());
+		// If the trackIngo is an album, will try to check if it have an attached
+		// picture in the database
+		if (trackInfo.getType().equals(ALBUM))
+			node.setAttribute("hasPicture", ATTACHED_PICTURE_DAO.hasPicture(trackInfo.getId()) + "");
+		return node;
+	}
+
+	@Override
+	public String execute() {
+		LOG.info("Getting Library TrackInfos");
+		try {
+			final Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+			final Element treeNode = doc.createElement("libTree");
+			final Collection<Track> tracks = getUser().getLibrary().getTracks();
+			for (final Track track : tracks) {
+				boolean exists = false;
+				final NodeList genreList = treeNode.getChildNodes();
+				final TrackInfo genre = track.getTrackInfo(GENRE);
+				final TrackInfo artist = track.getTrackInfo(ARTIST);
+				final TrackInfo album = track.getTrackInfo(ALBUM);
+				for (int i = 0; i < genreList.getLength(); i++) {
+					final Element genreNode = (Element) genreList.item(i);
+					if (genre != null && genreNode.getAttribute("value").equals(genre.getValue())) {
+						// If this genre already exists, will populate the corresponding
+						// node with
+						final NodeList artistLst = genreNode.getChildNodes();
+						for (int j = 0; j < artistLst.getLength(); j++) {
+							final Element artistNode = (Element) artistLst.item(j);
+							if (artistNode.getAttribute("value").equals(artist.getValue())) {
+								// If this artist already exists, will populate the
+								// corresponding node with
+								final NodeList albLst = artistNode.getChildNodes();
+								for (int k = 0; k < albLst.getLength(); k++)
+									if (((Element) albLst.item(k)).getAttribute("value").equals(album.getValue())) {
+										exists = true;
+										break;
+									}
+								if (!exists) {
+									artistNode.appendChild(makeNodeFromProp(doc, album));
+									exists = true;
+								}
+							}
+							if (exists)
+								break;
+						}
+						if (!exists) {
+							genreNode.appendChild(makeNodeFromProp(doc, artist)).appendChild(makeNodeFromProp(doc, album));
+							exists = true;
+						}
+					}
+					if (exists)
+						break;
+				}
+				if (genre != null && !exists)
+					treeNode.appendChild(makeNodeFromProp(doc, genre)).appendChild(makeNodeFromProp(doc, artist)).appendChild(
+							makeNodeFromProp(doc, album));
+			}
+			doc.appendChild(treeNode);
+			return outputDocument(doc);
+		} catch (final Exception e) {
+			LOG.fatal("Error during Library TrackInfo XML creation", e);
+		}
+		return NONE;
+	}
+}
---
+++ yass-webapp/src/main/java/org/yass/struts/track/Play.java
@@ -0,0 +1,94 @@
+/*
+ Copyright (c) 2008 Sven Duzont sven.duzont@gmail.com> All rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is furnished
+ to do so, subject to the following conditions: The above copyright notice
+ and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS",
+ WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.yass.struts.track;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+import org.yass.YassConstants;
+import org.yass.domain.Track;
+import org.yass.struts.YassAction;
+
+public class Play extends YassAction implements YassConstants {
+
+	private static final long serialVersionUID = 3411435373847531163L;
+	private int id;
+
+	@Override
+	public String execute() {
+		final Track track = TRACK_DAO.getById(id);
+		if (track == null) {
+			LOG.error("The Track id:" + id + " cannot be found in persistent store");
+			return ERROR;
+		}
+		final File trackFile = new File(track.getPath());
+		OutputStream out = null;
+		InputStream fis = null;
+		try {
+			final HttpServletResponse response = ServletActionContext.getResponse();
+			response.setContentType("audio/mpeg");
+			response.setContentLength(new Long(trackFile.length()).intValue());
+			if (LOG.isInfoEnabled())
+				LOG.info("Streaming Track id:" + id + ", path:" + track.getPath());
+			fis = new FileInputStream(trackFile);
+			out = response.getOutputStream();
+			final byte[] buf = new byte[4 * 1024]; // 4K buffer
+			int bytesRead;
+			while ((bytesRead = fis.read(buf)) != -1)
+				out.write(buf, 0, bytesRead);
+			response.flushBuffer();
+		} catch (final FileNotFoundException e) {
+			LOG.error("", e);
+			return ERROR;
+		} catch (final IOException e) {
+			LOG.error("", e);
+			return ERROR;
+		} finally {
+			if (fis != null)
+				try {
+					fis.close();
+				} catch (final IOException e) {
+				}
+			if (out != null)
+				try {
+					out.close();
+				} catch (final IOException e) {
+				}
+		}
+		return NONE;
+	}
+
+	/**
+	 * @param keywords
+	 *          the keywords to set
+	 */
+	public final void setId(final int id) {
+		this.id = id;
+	}
+}
